198c108133b70b136ff8f67c2c022b7497b5566f
Kevin Kunzmann <kevinkunzmann@gmx.net>
2021-02-04 10:28:28 +0000

    ...

--- test/tests/test_tests.jl
+++ test/tests/test_tests.jl
@@ -6,7 +6,7 @@ lfd1 = LogPropTest("lfd-1", 5.0, 1/12, .997)
 lfd2 = LogGLMTest("lfd-2", 1e5, 2.0, .5, -15.0, .997)
 for i in 1:30
     step!.(individuals)
-    # conduct_test!.(lfd1, individuals)
+    conduct_test!.(lfd1, individuals)
     conduct_test!.(lfd2, individuals)
 end
 
