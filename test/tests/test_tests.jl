276e14edbec0e64527b7fbba68fe98dccdfae241
Kevin Kunzmann <kevinkunzmann@gmx.net>
2021-02-23 11:10:33 +0000

    testing FixedTest

--- test/tests/test_tests.jl
+++ test/tests/test_tests.jl
@@ -22,10 +22,17 @@ lfd = FixedTest("lfd", .5, 1.0; ar_window = 2, ar_coefficient = 0.75)
 step!(individual)
 @test get_probability_positive(lfd, individual) == 0.5
 conduct_test!(lfd, individual)
-individual.test_log_result[1] = true # guarantee that te test is positive
+individual.test_log_result[1] = true # guarantee that the test is positive
 @test get_probability_positive(lfd, individual) == 0.75 + 0.25*0.5
 step!(individual)
 step!(individual)
 @test get_probability_positive(lfd, individual) == 0.75 + 0.25*0.5
 step!(individual)
 @test get_probability_positive(lfd, individual) == 0.5
+
+
+
+pcr = FixedTest("pcr", 0.975, 1.0; lod = 300.0)
+@test get_probability_positive(pcr, 150) == 0.0
+@test get_probability_positive(pcr, 300) == 0.975
+@test get_probability_positive(pcr, 1e11) == 0.975
