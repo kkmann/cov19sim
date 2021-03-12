3fa87f84e5af6935689441dbc9d3b7f2e33fba6c
Kevin Kunzmann <kevinkunzmann@gmx.net>
2021-03-12 14:07:56 +0000

    added test case

--- test/tests/test_individual.jl
+++ test/tests/test_individual.jl
@@ -12,3 +12,5 @@ indv1 = Individual(dm, 0.01; a = 2/30, b = 1/30)
 println([Individual(dm, 0.01; a = 2/3, b = 1/3).compliance for i = 1:10])
 
 @test indv1.current_day == 0
+
+im = LogPropInfectionModel(.01, 1.0)
